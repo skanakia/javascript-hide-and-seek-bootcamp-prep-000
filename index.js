0ddba5aa00a4712502eb4ac55a41537e7b61d1df
Sameer Kanakia <skanakia1@gmail.com>
2018-03-27 14:41:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  let nodes = document.querySelectorAll('#grand-node div')
+  let nodesArray = Array.from(nodes)
+  return nodesArray[nodesArray.length - 1]
+}
+
+function increaseRankBy(n) {
+  let nodes = document.querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < nodes.length; i++) {
+    nodes[i].innerHTML = parseInt(nodes[i].innerHTML) + n
+  }
+}
