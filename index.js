d68166ccf512f1367ffe943e234d2a7a27ee9dd3
Sameer Kanakia <skanakia1@gmail.com>
2018-03-27 14:33:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function nestedTarget() {
 function deepestChild() {
   let node = document.querySelectorAll('#grand-node div')
   let nodeArray = Array.from(node)
-  
+  return nodeArray[nodeArray.length - 1]
 }
