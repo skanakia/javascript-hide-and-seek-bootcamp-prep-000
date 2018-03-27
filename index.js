733f387dd171ecd77adf4616f3b656389b66cc1c
Sameer Kanakia <skanakia1@gmail.com>
2018-03-27 14:35:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.querySelector('#nested')
+  return document.querySelector('#nested .target')
 }
 
 function deepestChild() {
